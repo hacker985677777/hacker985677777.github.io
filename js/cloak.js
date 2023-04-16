36e1c6ea8a4a1fec281b5e6509dd0015b81e3633
RedNotSus <Jiayangc1@gmail.com>
2023-04-15 22:38:05 -0500

    fix apps

--- js/cloak.js
+++ js/cloak.js
@@ -80,7 +80,21 @@ function chatgpt() {
   //Open page
   page.open();
 }
-
+function cloak(link) {
+  //Creates new ABC
+  var page = new ABC({
+    type: "blank", //Blank, blob, or overwrite
+    url: "https://google.com", //Any url
+  });
+  //Set the type
+  page.setType("blank");
+  //Set the url
+  page.setUrl(link);
+  //Get iframe code
+  console.log(page.getCode());
+  //Open page
+  page.open();
+}
 function login() {
   const ans = document.getElementById("uranswer").value;
   if (ans == "") {
